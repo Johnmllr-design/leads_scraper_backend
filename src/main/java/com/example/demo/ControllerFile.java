40cf52587c4ef73ebe92e32e06d66c6c639e5084
John Miller <johnmiller@macbookpro.attlocal.net>
2026-02-18 02:05:48 -0800

    updating application.properties

--- src/main/java/com/example/demo/ControllerFile.java
+++ src/main/java/com/example/demo/ControllerFile.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
+@CrossOrigin(origins = "https://leads-scraper-frontend.vercel.app/")
 public class ControllerFile {
 
     private DatabaseORM orm;
